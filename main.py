7d6b663916d8e2686e1ee46898ee9ff23a351706
soly421 <sam.olyaei@gmail.com>
2025-03-25 01:08:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -107,4 +107,4 @@ def get_db():
 Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
